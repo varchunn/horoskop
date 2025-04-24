018091fe3c6a5e5e3ca38f1928265aff1411a57c
varchunn <varvaram186@gmail.com>
2025-04-24 20:07:10 +0300

    Initial Commit

---
+++ horoskop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  horoskop
+//
+//  Created by Варя Мінєнкова on 24.04.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
